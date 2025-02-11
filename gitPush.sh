8ced4b8e78c7998933bf2faeeed048b5a0f81b48
Rahul Pandare <pandare.rahul@gmail.com>
2025-02-11 01:13:58 -0600

    Updated Files

---
+++ gitPush.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Add all changes
+git add .
+
+# Commit changes with a fixed message
+git commit -m "Updated Files"
+
+# Detect the current branch (main or master)
+BRANCH=$(git symbolic-ref --short HEAD)
+
+# Push changes
+git push origin "$BRANCH"
+
+echo "Changes pushed to $BRANCH with message: 'Updated Files'"
